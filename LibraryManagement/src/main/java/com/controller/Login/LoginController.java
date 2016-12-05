e7826845283fc0425fbc4864784997cd9e91e978
user.email <LungPanda@gmail.com>
2016-12-05 23:57:25 +0700

    no

--- LibraryManagement/src/main/java/com/controller/Login/LoginController.java
+++ LibraryManagement/src/main/java/com/controller/Login/LoginController.java
@@ -48,17 +48,17 @@ public class LoginController {
 			StaffJDBC staffJDBC = (StaffJDBC) context.getBean("staffJDBC");
 			Staff staff = staffJDBC.getStaffByEmail(email);
 			if (staff == null) {
-				model.addAttribute("error", "Tên đăng nhập hoặc mật khẩu không chính xác");
+				model.addAttribute("error", "TÃªn Ä‘Äƒng nháº­p hoáº·c máº­t kháº©u khÃ´ng chÃ­nh xÃ¡c");
 				return "login";
 			}
 			if (password.equals(staff.getPassword())) {
 				if (!staff.isStatus()) {
-					model.addAttribute("error", "Tài khoản này đang bị khóa");
+					model.addAttribute("error", "TÃ i khoáº£n nÃ y Ä‘ang bá»‹ khÃ³a");
 					return "login";
 				} else {
 					request.getSession().setAttribute("staffSession", staff);
 					request.getSession().setAttribute("roleSession", 1);
-					redirectAtt.addFlashAttribute("success", "Đăng nhập thành công!");
+					redirectAtt.addFlashAttribute("success", "Ä�Äƒng nháº­p thÃ nh cÃ´ng!");
 					return "redirect:/thongtincanhan";
 				}
 			}
@@ -66,17 +66,17 @@ public class LoginController {
 			StudentJDBC studentJDBC = (StudentJDBC) context.getBean("studentJDBC");
 			Student student = studentJDBC.getStudentByEmail(email);
 			if (student == null) {
-				model.addAttribute("error", "Tên đăng nhập hoặc mật khẩu không chính xác");
+				model.addAttribute("error", "TÃªn Ä‘Äƒng nháº­p hoáº·c máº­t kháº©u khÃ´ng chÃ­nh xÃ¡c");
 				return "login";
 			}
 			if (password.equals(student.getPassword())) {
 				if (!student.isStatus()) {
-					model.addAttribute("error", "Tài khoản này đang bị khóa");
+					model.addAttribute("error", "TÃ i khoáº£n nÃ y Ä‘ang bá»‹ khÃ³a");
 					return "login";
 				} else {
 					request.getSession().setAttribute("studentSession", student);
 					request.getSession().setAttribute("roleSession", 2);
-					redirectAtt.addFlashAttribute("success", "Đăng nhập thành công!");
+					redirectAtt.addFlashAttribute("success", "Ä�Äƒng nháº­p thÃ nh cÃ´ng!");
 					return "redirect:/thongtincanhan";
 				}
 			}
